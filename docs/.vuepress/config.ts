9c2f5a677cc4647c38bb31807f8923df56c0306b
Hendrik Erz <hendrik@zettlr.com>
2026-08-02 16:39:56 +0200

    fix: Explicitly configure plugins

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,7 @@ import { defaultTheme } from "@vuepress/theme-default"
 import { defineUserConfig } from "vuepress"
 import { searchPlugin } from "@vuepress/plugin-search"
 import { redirectPlugin } from "@vuepress/plugin-redirect"
+import { GitContributorInfo } from "@vuepress/plugin-git"
 import configEn from "../../config/en.config"
 import configDe from "../../config/de.config"
 
@@ -17,6 +18,73 @@ export default defineUserConfig({
     locales: {
       ...configEn,
       ...configDe
+    },
+    themePlugins: {
+      activeHeaderLinks: true,
+      backToTop: true,
+      copyCode: {
+        showInMobile: true
+      },
+      git: {
+        contributors: {
+          avatar: false,
+          transform (c) {
+            const modifiedContributors: GitContributorInfo[] = []
+            for (const contributor of c) {
+              if (contributor.name === "nathanlesage") {
+                contributor.name = "Hendrik Erz"
+              }
+              
+              const existingContrib = modifiedContributors.find(x => x.name === contributor.name)
+              if (existingContrib === undefined) {
+                // Do not provide email addresses to the public page
+                contributor.email = ""
+                modifiedContributors.push(contributor)
+              }
+            }
+
+            return modifiedContributors
+          }
+        }
+      },
+      hint: {
+        alert: true,
+        hint: true
+      },
+      linksCheck: true,
+      prismjs: {
+        whitespace: "leading"
+      },
+      seo: {
+        hostname: "docs.zettlr.com",
+        author: {
+          name: "Hendrik Erz",
+          url: "https://www.zettlr.com/",
+          email: "info@zettlr.com"
+        },
+        autoDescription: true,
+        fallBackImage: undefined, // TODO
+        jsonLd: {
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "name": "Zettlr",
+          "url": "https://www.zettlr.com",
+          "logo": "https://docs.zettlr.com/logo.png",
+          "sameAs": [
+            "https://www.twitter.com/zettlr",
+            "https://facebook.com/zettlrapp",
+            "https://www.youtube.com/channel/UCso4Cjcv5gNt9LVBEZWi1cw",
+            "https://www.linkedin.com/company/zettlr",
+            "https://github.com/Zettlr/"
+          ]
+        } as any
+      },
+      sitemap: {
+        hostname: "docs.zettlr.com",
+        sitemapFilename: "sitemap.xml",
+        changefreq: "weekly"
+      },
+      tab: false
     }
   }),
   dest: "./build",
